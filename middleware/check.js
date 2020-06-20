17cca35eb0936220fec57d6f2cade505d7b404e3
Hammadsoloman <hammadali95@yahoo.com>
2020-06-20 14:21:54 +0300

    still work on the models

---
+++ middleware/check.js
@@ -0,0 +1,26 @@
+function getModel(req, res, next) {
+  let model = req.params.model;
+  let md = require(`../lib/models/products/products-model`);
+  let mdC = require(`../lib/models/categories/categories-model`);
+
+
+
+  console.log('getModel', req.body);
+
+
+  switch (model) {
+  case 'categories':
+    req.model = mdC;
+    next();
+    return;
+  case 'products':
+    req.model = md;
+    next();
+    return;
+  default:
+    next('Invalid Model');
+    return;
+  }
+}
+
+module.exports = getModel;
